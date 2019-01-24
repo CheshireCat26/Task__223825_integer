d0f17084f51d0559e48959a8f3eca2d996ef059c
CheshireCat26 <nail160306@gmail.com>
2019-01-24 16:02:52 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void heapSort(vector<int> &mass);
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
